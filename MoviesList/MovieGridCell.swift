024c82903604b58342b8bb3b7785b5773b755183
Michael <caseymichaelt1893@gmail.com>
2021-02-26 01:05:50 -0500

    Version 2

---
+++ MoviesList/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  MoviesList
+//
+//  Created by Michael Casey on 2/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
